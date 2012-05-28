9938713bd3f2e121e3bd51ae3e63d1ddf8fb4a19
Matthew Baxter-Reynolds (@mbrit) <mbrit@mbrit.com>
2012-05-28 21:41:57 +0100

    Final

--- Part3-LocalData/StreetFooClient/StreetFooClient/Objects/AppRuntime.cs
+++ Part3-LocalData/StreetFooClient/StreetFooClient/Objects/AppRuntime.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -44,6 +45,5 @@ namespace StreetFooClient
         {
             return new SQLiteAsyncConnection(SystemDatabaseConnectionString);
         }
-
     }
 }
